ee18acd448c48a18540404000e7eaaff932a2a85
yamap55 <tetuya120@hotmail.com>
2016-02-25 20:25:43 +0900

    add 'Matching Specific Characters'

---
+++ Regex/Character_Class/Matching_Specific_Characters.java
@@ -0,0 +1,9 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[1-3][0-2][0xs][03Aa][xsu][.,]$"); // Use \\ instead of using \ 
+    
+    }
+}
