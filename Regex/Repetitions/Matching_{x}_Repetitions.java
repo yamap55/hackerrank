ac33d7c5304d560ae38dc8f9f27298ed5d34ddfc
yamap55 <tetuya120@hotmail.com>
2016-02-26 23:55:06 +0900

    add 'Matching {x} Repetitions'

---
+++ Regex/Repetitions/Matching_{x}_Repetitions.java
@@ -0,0 +1,9 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[02468a-zA-Z]{40}[13579\\s]{5}$"); // Use \\ instead of using \ 
+    
+    }
+}
