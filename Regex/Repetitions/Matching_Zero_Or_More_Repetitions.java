eace1502fc96d840050241bb43bcf9e72c89e24e
yamap55 <tetuya120@hotmail.com>
2016-02-28 22:56:38 +0900

    add 'Matching Zero Or More Repetitions'

---
+++ Regex/Repetitions/Matching_Zero_Or_More_Repetitions.java
@@ -0,0 +1,9 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[0-9]{2,}[a-z]*[A-Z]*$"); // Use \\ instead of using \ 
+    
+    }
+}
